45632392f40e5570e26bee91ef9261b24b20b037
Jorge Mateus <jorge.mateus19@hotmail.com>
2025-11-18 22:25:11 -0300

    Inclusão de Material

--- src/data/products.ts
+++ src/data/products.ts
@@ -13,13 +13,13 @@ export const products: Product[] = [
   },
   {
     id: 2,
-    brand: "Lenovo",
-    model: "IdeaPad 3i",
-    processor: "Intel Core i7 11ª geração",
-    ram: "16GB",
-    storage: "512GB SSD",
-    price: 4899,
-    image: "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?w=800&h=600&fit=crop"
+    brand: "Dell",
+    model: "Alienware 16 Aurora",
+    processor: "Intel® Core™ 7-240H, 10 núcleos",
+    ram: "32GB",
+    storage: "1TB SSD",
+    price: 5800,
+    image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/alienware-notebooks/ac16250/media-gallery/laptop-alienware-ac16250-gallery-6.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=519&qlt=100,1&resMode=sharp2&size=519,402&chrss=full"
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ export const products: Product[] = [
     ram: "8GB",
     storage: "256GB SSD",
     price: 3299,
-    image: "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?w=800&h=600&fit=crop"
+    image: "https://p2-ofp.static.pub//fes/cms/2024/12/26/bipc0oog4dbfih8upuzuz0a5dfagki033513.png?width=400&height=400"
   },
   {
     id: 4,
@@ -39,7 +39,7 @@ export const products: Product[] = [
     ram: "8GB",
     storage: "512GB SSD",
     price: 3799,
-    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=800&h=600&fit=crop"
+    image: "https://p2-ofp.static.pub//fes/cms/2024/12/26/sg6mjt9gpm2zol49crwqza3y6z723i528922.png?width=400&height=400"
   },
   {
     id: 6,
@@ -59,7 +59,7 @@ export const products: Product[] = [
     ram: "8GB",
     storage: "256GB SSD",
     price: 4299,
-    image: "https://images.unsplash.com/photo-1547082299-de196ea013d6?w=800&h=600&fit=crop"
+    image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/alienware-notebooks/ac16250/media-gallery/laptop-alienware-ac16250-gallery-9.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=518&qlt=100,1&resMode=sharp2&size=518,402&chrss=full"
   },
   {
     id: 9,
@@ -69,7 +69,7 @@ export const products: Product[] = [
     ram: "16GB",
     storage: "512GB SSD",
     price: 5299,
-    image: "https://images.unsplash.com/photo-1603302576837-37561b2e2302?w=800&h=600&fit=crop"
+    image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/alienware-notebooks/ac16250/media-gallery/laptop-alienware-ac16250-gallery-4.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=613&qlt=100,1&resMode=sharp2&size=613,402&chrss=full"
   },
   {
     id: 10,
@@ -79,7 +79,7 @@ export const products: Product[] = [
     ram: "8GB",
     storage: "512GB SSD",
     price: 3999,
-    image: "https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=800&h=600&fit=crop"
+    image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3530-intel/media-gallery/black/notebook-inspiron-15-3530-nt-plastic-black-gallery-10.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=593&qlt=100,1&resMode=sharp2&size=593,402&chrss=full"
   },
   {
     id: 11,
@@ -89,6 +89,6 @@ export const products: Product[] = [
     ram: "16GB",
     storage: "512GB SSD",
     price: 8999,
-    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&h=600&fit=crop"
+    image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3530-intel/media-gallery/black/notebook-inspiron-15-3530-nt-plastic-usbc-data-black-gallery-4.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=642&qlt=100,1&resMode=sharp2&size=642,402&chrss=full"
   }
 ];
